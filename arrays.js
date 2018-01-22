8744bd84d0bfaf722f0e17b424967018c5c4a7bf
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-22 18:25:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
   
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
